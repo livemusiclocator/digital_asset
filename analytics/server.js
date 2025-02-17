23f73cff67ef9b020113af80f8be4586e87e9e0a
rvklenora <rvklenora@gmail.com>
2025-02-17 18:51:47 +1100

    server.js

--- analytics/server.js
+++ analytics/server.js
@@ -5,12 +5,24 @@ const PORT = process.env.PORT || 3000; // Default to port 3000
 
 // Middleware to log every request
 app.use((req, res, next) => {
-    console.log(`QR Code Accessed: ${req.path}`);
-    res.send(`You accessed: ${req.path}`);
-    next();
+    const cleanPath = req.path.replace(/^\/+/, ''); // Remove leading slash
+    console.log(`QR Code Accessed: ${cleanPath}`);
+
+    // Google Analytics integration can be added here
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+
+    const url = `https://lml.live/?dateRange=thisWeek&venues=${cleanPath}`;
+    res.redirect(url);
 });
 
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}/`);
 });
+
